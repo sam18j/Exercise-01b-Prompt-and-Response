b91cb694e24c04ded40226fcd054e0307d3a9d0d
Samaree Barnes <barnesay@iu.edu>
2021-08-31 23:15:00 -0400

    Update main.py
    hi

--- main.py
+++ main.py
@@ -4,3 +4,10 @@ assert sys.version_info >= (3,9), "This script requires at least Python 3.9"
 
 passage_name = "West of House"
 passage_text = "This is an open field west of a white house, with a boarded front door."
+
+print("You are at the west of house" + passage_name)
+print(passage_text)
+response = input("what would you like to do?")
+print(response)
+if response == "go north":
+    print("Good job")
